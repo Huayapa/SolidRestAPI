8cb12793415523c142db3c009183c1854442882f
josue Huayapa <huayapajosue0@gmail.com>
2026-02-18 20:30:23 -0500

    fix path code error

--- src/modules/money/domain/errors/InvalidExchangeRateError.ts
+++ src/modules/money/domain/errors/InvalidExchangeRateError.ts
@@ -1,7 +1,7 @@
 import { DomainError } from "#shared/domain/errors/DomainError.ts"
 
 export class InvalidExchangeRateError extends DomainError {
-  readonly code:string = 'INVALID_EXCHANGE_RATE_ERROR'
+  readonly code:string = 'INVALID_EXCHANGE_RATE'
   constructor(rate: number) {
     super(`Coversión de cambio invalido: ${rate}, el cambio debe ser positivo`)
   }
