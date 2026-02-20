4950115706af10adea56e73dad21659a0d0338f1
josue Huayapa <huayapajosue0@gmail.com>
2026-02-19 19:36:46 -0500

    fix code error ExchangeRateNotFoundError and PaymentAlReadyExistsError

--- src/modules/money/domain/errors/ExchangeRateNotFoundError.ts
+++ src/modules/money/domain/errors/ExchangeRateNotFoundError.ts
@@ -4,7 +4,7 @@ import { DomainError } from "#shared/domain/errors/DomainError.ts"
 
 
 export class ExchangeRateNotFoundError extends DomainError {
-  readonly code:string = 'ENCHANGE_RATE_NOT_FOUND'
+  readonly code:string = 'EXCHANGE_RATE_NOT_FOUND'
   constructor(from: Currency, to:Currency) {
     super(`No se encontro la tasa de cambio: ${from.getValue()} - ${to.getValue()}`)
   }
--- src/modules/payments/domain/errors/PaymentAlReadyExistsError.ts
+++ src/modules/payments/domain/errors/PaymentAlReadyExistsError.ts
@@ -1,7 +1,7 @@
 import { DomainError } from "#shared/domain/errors/DomainError.ts"
 
 export class PaymentAlReadyExistsError extends DomainError {
-  readonly code:string = 'PAYMENT_AL_READY_EXISTS'
+  readonly code:string = 'PAYMENT_ALREADY_EXISTS'
   constructor(id:string) {
     super(`Este pago ${id} ya existe`)
   }
